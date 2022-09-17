cbb66dbed0b1a7c6c2f8d4e1f3ed62d269e874fb
BurakBozan <burakbozantr@gmail.com>
2022-09-17 13:52:17 +0900

    add main mathfunctions

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
  //main operations/functions
 
+ function add(a,b) {
+    return a+b;
+ }
+
+ function subtract (a,b) {
+    return a-b;
+ }
+
+ function multiply (a,b) {
+    return a*b;
+ }
+
+ function divide (a,b) {
+    return a/b;
+ }
+
+ 
