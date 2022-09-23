4cc1bc8b36ed2e8c8ffc13d794b04ef2e22db621
BurakBozan <burakbozantr@gmail.com>
2022-09-23 23:41:44 +0900

    change the way getting component from html

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
- //main math functions
+let operator = '';
+let previousValue = '';
+let currentValue = '';
 
- function add(a,b) {
-    return a+b;
- }
+//main math functions
 
- function subtract (a,b) {
-    return a-b;
- }
+function add(a, b) {
+  return a + b;
+}
 
- function multiply (a,b) {
-    return a*b;
- }
+function subtract(a, b) {
+  return a - b;
+}
 
- function divide (a,b) {
-    return a/b;
- }
+function multiply(a, b) {
+  return a * b;
+}
 
- 
+function divide(a, b) {
+  return a / b;
+}
 
- //operate function that takes an operator and 2 numbers
 
- function operate (operator, a, b) {
-    if (operator == 'add') {
-        return add(a,b);
-    } else if (operator == 'subtract') {
-        return subtract(a,b);
-    } else if (operator == 'multiply') {
-        return multiply(a,b);
-    } else return divide(a,b);
- }
+
+//operate function that takes an operator and 2 numbers
+
+function operate(operator, a, b) {
+  if (operator == 'add') {
+    return add(a, b);
+  } else if (operator == 'subtract') {
+    return subtract(a, b);
+  } else if (operator == 'multiply') {
+    return multiply(a, b);
+  } else return divide(a, b);
+};
+
+
+// components from html
+
+let clear = document.querySelector('#clear-button');
+let equal = document.querySelector('#result-button');
+
+let numbers = document.querySelectorAll('.numeric-btn');
+let operators = document.querySelectorAll('.operation-btn');
+
+let topDisplay = document.querySelector('.top-display');
+let bottomDisplay = document.querySelector('.bottom-display');
+
